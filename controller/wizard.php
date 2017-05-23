47d67da27ae7a77dac68dfc1595d7cdfec476ceb
AB <andreabuffasbru@gmail.com>
2017-05-23 10:22:36 +0200

    current state/page check added. Some clean up.

--- controller/wizard.php
+++ controller/wizard.php
@@ -4,25 +4,25 @@ interface iState {
 	public function process($method, $page);
 }
 
-abstract class State implements iState {
-	protected $context;
-}
+abstract class State implements iState {}
 
 class StartState extends State {
+	const NAME = 'start';
 
 	public function process($method, $page) {
 		//reset all session data
 		$_SESSION['wizState'] = new StyleState();
-		return header('Location: index.php?page=style');
+		return header('Location: index.php?page='.StyleState::NAME);
 	}
 }
 
 class StyleState extends State {
+	const NAME = 'style';
 
 	public function process($method, $page) {
-		if ($page !== 'style') {
-			return header('Location: index.php?page=style');
-		} 
+		if ($page !== self::NAME) {
+			return header('Location: index.php?page='.self::NAME);
+		}
 		if ($method === 'GET') {
 			require_once(__DIR__.'/../view/wizard/wizView.php');
 			$this->view = new WizView();
@@ -34,23 +34,27 @@ class StyleState extends State {
 			if (isset($_POST['style'])) {
 				$_SESSION['style'] = $_POST['style'];
 				$_SESSION['wizState'] = new PanoramaState();
-				return header('Location: index.php?page=panorama');
+				return header('Location: index.php?page='.PanoramaState::NAME);
 			}
 		}
 	}
 }
 
 class PanoramaState extends State {
+	const NAME = 'panorama';
 
 	public function process($method, $page) {
-		require_once(__DIR__.'/../view/wizard/wizView.php');
-		$this->view = new WizView();
-		$this->view->setTplParam('HOST', HOST);
-		$this->view->setTplParam('METHOD', METHOD);
-		$this->view->setTplParam('APP', APP);
+		if ($page !== self::NAME) {
+			return header('Location: index.php?page='.self::NAME);
+		}		
 		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
+			
 		} else {
+			require_once(__DIR__.'/../view/wizard/wizView.php');
+			$this->view = new WizView();
+			$this->view->setTplParam('HOST', HOST);
+			$this->view->setTplParam('METHOD', METHOD);
+			$this->view->setTplParam('APP', APP);
 			return $this->view->choosePanorama();
 		}
 	}
@@ -58,22 +62,22 @@ class PanoramaState extends State {
 
 class OpeningState extends State {
 
-	public function process($method) {}
+	public function process($method, $page) {}
 }
 
 class ConfigState extends State {
-	public function process($method) {}
+	public function process($method, $page) {}
 }
 
 class WizardFactory {
 	protected $controller;
 	public function __construct($pageName) {
 		switch ($pageName) {
-			case 'start':
-			case 'style':
-			case 'panorama':
-			case 'opening':
-			case 'config':
+			case StartState::NAME:
+			case StyleState::NAME:
+			case PanoramaState::NAME:
+			case OpeningState::NAME:
+			case ConfigState::NAME:
 				$this->controller = new Wizard($pageName);
 			break;
 			default:
@@ -90,17 +94,17 @@ class Wizard extends ControllerBase {
 
 	private $currentState;
 
-	private $stepList = array('start' => '');
+	private $pageName;
 
 	public function __construct($pageName) {
 		session_start();
-		if ($_SESSION['wizState'] && ($pageName !== 'start')) {
+		if ($_SESSION['wizState'] && ($pageName !== StartState::NAME)) {
 			$this->currentState = $_SESSION['wizState'];
-			//var_dump($this->currentState);
 		} else{
 			$this->currentState = new StartState();
 			$_SESSION['wizState'] = $this->currentState;
 		}
+		$this->pageName = $pageName;
 	}
 
 	public function processRequest() {
@@ -109,7 +113,7 @@ class Wizard extends ControllerBase {
 			case 'GET':
 			case 'POST':
 				//var_dump($this->currentState);
-				return $this->currentState->process($method, $page);
+				return $this->currentState->process($method, $this->pageName);
 			break;
 		}
 	}
